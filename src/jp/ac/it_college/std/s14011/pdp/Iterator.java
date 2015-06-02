0b6a7bf4d0b5fa3657b9b3d879d0f85c0e2a8dd3
s14011 <s14011@std.it_college.ac.jp>
2015-06-02 11:22:34 +0900

    initial commit

---
+++ src/jp/ac/it_college/std/s14011/pdp/Iterator.java
@@ -0,0 +1,8 @@
+package jp.ac.it_college.std.s14011.pdp;
+
+/**
+ * Created by s14011 on 15/06/02.
+ */
+public interface Iterator {
+    public abstract boolean hasNext();
+}
---
+++ src/jp/ac/it_college/std/s14011/pdp/Main.java
@@ -0,0 +1,8 @@
+package jp.ac.it_college.std.s14011.pdp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
